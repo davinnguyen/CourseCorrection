39a42c413739e764ce6678447a4a473121a34695
Davin <davinnguyen@yahoo.com>
2025-06-17 12:25:46 -0700

    Initial commit of CourseCorrection project

---
+++ CourseCorrection/CourseCorrectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseCorrectionApp.swift
+//  CourseCorrection
+//
+//  Created by Davin on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CourseCorrectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CourseCorrectionTests/CourseCorrectionTests.swift
@@ -0,0 +1,16 @@
+//
+//  CourseCorrectionTests.swift
+//  CourseCorrectionTests
+//
+//  Created by Davin on 6/17/25.
+//
+
+import Testing
+
+struct CourseCorrectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
